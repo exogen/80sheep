4f5dfc57458fbfe1d9968435be01449c3810efaa
Brian Beck <exogen@gmail.com>
2009-02-22 19:55:42 -0500

    Added two MessagePart dispatching tests.

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,5 +1,5 @@
 from nose.tools import assert_raises
-from libsheep.protocol import Message, MessagePart
+from libsheep.protocol import Message, MessagePart, Context, Command
 
 def test_message_args_set_message_parts():
     m = Message('H', 'SUP')
@@ -10,3 +10,12 @@ def test_message_parts_have_arg_codes():
     m = Message('H', 'SUP')
     assert m.context.code == 'H'
     assert m.command.code == 'SUP'
+
+def test_h_context_creates_h_instance():
+    from libsheep.protocol import CIH
+    context = Context('H')
+    assert isinstance(context, CIH)
+
+def test_unknown_context_creates_generic_instance():
+    context = Context('Z')
+    assert type(context) is Context
